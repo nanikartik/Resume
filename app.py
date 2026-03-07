7de1fa62bce9b8e11413a816f5ab1d349bc100ac
nanikartik <161662541+nanikartik@users.noreply.github.com>
2026-03-07 23:44:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,9 +27,9 @@ lemmatizer = WordNetLemmatizer()
 @st.cache_resource
 def load_model():
 
-    model = joblib.load("nb_model.pkl")
-    vectorizer = joblib.load("vectorizer_nb.pkl")
-    label_encoder = joblib.load("label_encoder_nb.pkl")
+    model = joblib.load("svm_model.pkl")
+    vectorizer = joblib.load("svm_vectorizer.pkl")
+    label_encoder = joblib.load("label_encoder_svm.pkl")
 
     return model, vectorizer, label_encoder
 
